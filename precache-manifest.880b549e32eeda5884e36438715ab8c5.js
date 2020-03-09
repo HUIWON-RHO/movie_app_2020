42f25639e2a46b712ed81252f8b1a1e7b6df8da0
HUIWON-RHO <HUIWON-RHO@users.noreply.github.com>
2020-03-09 23:23:14 +0100

    Updates

---
+++ precache-manifest.880b549e32eeda5884e36438715ab8c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cb1117a35fe57756c19e8ff0eeb0063",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "81c78f404e2a70a0eb0d",
+    "url": "/movie_app_2020/static/css/main.717c8c1f.chunk.css"
+  },
+  {
+    "revision": "47c8bb3d241ed711a8e9",
+    "url": "/movie_app_2020/static/js/2.ca606c69.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_app_2020/static/js/2.ca606c69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81c78f404e2a70a0eb0d",
+    "url": "/movie_app_2020/static/js/main.5db22027.chunk.js"
+  },
+  {
+    "revision": "5b9de689d76c950937df",
+    "url": "/movie_app_2020/static/js/runtime-main.db2dcdc9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie_app_2020/precache-manifest.a95fcd9919e5983d93f5e683890553a7.js"
+  "/movie_app_2020/precache-manifest.880b549e32eeda5884e36438715ab8c5.js"
 );
 
 self.addEventListener('message', (event) => {
